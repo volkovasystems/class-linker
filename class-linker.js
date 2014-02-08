631a36357af0cba3da803ec08af107e7d1e99d0e
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-02-08 22:49:36 +0800

    Added class Linker.

---
+++ class-linker.js
@@ -0,0 +1,17 @@
+/*:
+
+*/
+
+Linker = function Linker( name ){
+	
+};
+
+Linker.prototype.link = function link( linkA, linkB ){
+	/*:
+		@method-documentation:
+			This accepts a linkable engine that connects
+				to another linkable engine.
+			The linkable engines do bi-directional communication.
+		@end-method-documentation
+	*/
+};
